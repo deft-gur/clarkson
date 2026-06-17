bff15f07ea05980185392b34d14a394a43c300a6
deft gur <ziwenhehe@gmail.com>
2026-06-17 01:17:55 -0400

    fix epsilon-net bug

--- src/clarkson.jl
+++ src/clarkson.jl
@@ -363,7 +363,7 @@ module clarkson
   end
 
   function getEpsSize(n::Int64, epsilon::Float64, delta::Float64)
-    return Int64(ceil(min(8 * n*log(n/epsilon)/epsilon, log(1/delta)/epsilon)))
+    return Int64(ceil(max(8 * n*log(n/epsilon)/epsilon, log(1/delta)/epsilon)))
   end
 
   # Clarkson(model)
